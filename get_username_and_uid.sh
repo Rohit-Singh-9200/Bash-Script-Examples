ab9aa32794f9a7c284d4de5c69f904177e94c092
Rohit Singh <rohit.9200@gmail.com>
2025-05-17 15:50:40 +0000

    updated comment in the script

--- get_username_and_uid.sh
+++ get_username_and_uid.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-##########################################################################
-# This Script is to get all the general system username and its UID
-##########################################################################
+############################################################################################
+# This Script is to list of general users on the system and their corresponding user IDs
+############################################################################################
 #set -x
 set -e
 set -o pipefail
 
 username=$(sudo cat /etc/passwd | awk -F: '($3>999){ print $1,$3 }')
 
-# ($3>999) is because by default user UID are more than 1000 for general users
+# ($3>999) is because by default regular user UID are more than 1000
 
 echo "######### Here are the list of general users and their OID in this system ##########"
 echo "$username"
