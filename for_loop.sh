87f16c4d4539ac1b2cbd4a96620f88db09916ad8
rohit-singh-9200 <root@osboxes.org>
2025-05-23 09:56:22 -0400

    Updated comments

--- for_loop.sh
+++ for_loop.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+###############################################################
+# For Loop example to print individual letter in a word
+##############################################################
+
+# Prompt the user to enter word and store it into the 'word' variable
 read -p "Enter the Word: " word
 
+# Get the length of the entered word
 len_of_word=${#word}
 
+# Print the length of the word
 echo "Length of the word is: " $len_of_word
 
+# Print each letter in a word
 for (( i=0; i<$len_of_word; i++ ))
 do
   echo ${word:$i:1}
